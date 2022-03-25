5dc8c4bcec9a822813dad5df6bfc02ac0a84da24
jr600 <89954493+jr600@users.noreply.github.com>
2022-03-25 14:06:12 +0300

    pension and national insurance calculation

--- estimeated_tax_calculator_2022.py
+++ estimeated_tax_calculator_2022.py
@@ -1,41 +1,67 @@
-tax_levels_2022 = [[6_450, 0.1], [9_240, 0.14], [14_840, 0.2], [20_620, 0.31], [42_910, 0.35], [float('inf'), 0.47]]
-credit_point_value = 223
+from typing import Any, Generator, List, Union
 
 
-def level_gen(levels):
+pension_worker_deduct_rate: float = 0.06
+credit_point_value: int = 223
+tax_level = List[Union[int, float]]
+income_tax_2022: list[tax_level] = [[6_450, 0.1], [9_240, 0.14], [14_840, 0.2], [20_620, 0.31], [42_910, 0.35],
+                                    [float('inf'), 0.47]]
+national_insurance_worker_2022: list[tax_level] = [[6_331, 0.035], [45_075, 0.12], [float('inf'), 0]]
+
+
+def level_gen(levels: list) -> Generator[tax_level, None, None]:
     for level in levels:
         yield level
 
 
-tax_levels = level_gen(tax_levels_2022)
-omitted = 0
-tax = 0
+income_tax_levels: Generator[tax_level, None, None] = level_gen(income_tax_2022)
+national_insurance_levels: Generator[tax_level, None, None] = level_gen(national_insurance_worker_2022)
+omitted: Union[int, float] = 0
+tax: Union[int, float] = 0
 
 
-def calc_tax(income, credit_points):
+def tax_to_deduct_from_salary(income: Union[int, float],
+                              tax_levels: Generator[tax_level, None, None], credit_points: Union[int, float] = 0)\
+        -> Union[int, float]:
     global omitted
     global tax
-    current_level = next(tax_levels)
-    if income > current_level[0]:
+    current_level: tax_level = next(tax_levels)
+    if income >= current_level[0]:
         if not omitted:
             tax += current_level[0] * current_level[1]
             omitted += current_level[0]
-            return calc_tax(income, credit_points)
+            return tax_to_deduct_from_salary(income, tax_levels, credit_points)
         elif omitted:
             tax += (current_level[0] - omitted) * current_level[1]
             omitted += (current_level[0] - omitted)
-            return calc_tax(income, credit_points)
-    elif income == current_level[0]:
-        tax += current_level[0] * current_level[1]
+            return tax_to_deduct_from_salary(income, tax_levels, credit_points)
     else:
         tax += (income - omitted) * current_level[1] - (credit_points * credit_point_value)
         tax = round(tax)
         if tax >= 0:
             return tax
-        else:
-            return
+    return 0
 
-user_income = input("Please enter the total sum of income: ")
-user_credit_points = input("Please enter the number of credit points: ")
-print(f"\nEstimated tax to be deducted from salary: \
-{calc_tax(int(user_income) if user_income else 0 , float(user_credit_points) if user_credit_points else 0)} ILS!")
+
+user_income: Any = input("Please enter the total sum of income: ")
+user_credit_points: Any = input("Please enter the number of credit points: ")
+
+national_insurance_deduction: Union[int, float] =\
+    tax_to_deduct_from_salary(int(user_income) if user_income else 0, national_insurance_levels)
+
+income_tax_deduction: Union[int, float] =\
+    tax_to_deduct_from_salary(int(user_income) if user_income else 0, income_tax_levels,
+                              float(user_credit_points) if user_credit_points else 0)
+
+pension_deduction: float = pension_worker_deduct_rate * float(user_income)
+
+print(f"""
+Gross Salary: {user_income}
+National Insurance Deduction: {national_insurance_deduction}
+Income Tax Deduction: {income_tax_deduction}
+Pension Deduction : {pension_deduction}
+
+Net Salary: {float(user_income) - float(national_insurance_deduction)
+             - (income_tax_deduction if income_tax_deduction else 0) - pension_deduction}
+
+""")
